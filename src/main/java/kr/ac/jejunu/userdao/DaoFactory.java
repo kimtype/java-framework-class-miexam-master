9f4fa3d5c2ed5540ba317c744f5a9ac0c1ff3893
kimtype <kimtybe@naver.com>
2020-05-14 17:28:39 +0900

    (6) DataSource로 변경

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,16 +1,41 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
-@Bean
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
+    @Bean
     public UserDao userDao() {
-        return new UserDao(ConnectionMaker());
+        return new UserDao(dataSource());
     }
-@Bean
-public JejuConnectionMaker ConnectionMaker() {
-        return new JejuConnectionMaker();
+    @Bean
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+
+    return dataSource;
     }
 }
